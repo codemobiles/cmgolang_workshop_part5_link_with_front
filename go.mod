579a3f7467b9b172db481a076a54836eed3b3a50
codemobiles <chaiyasit.t@gmail.com>
2020-10-03 10:47:39 +0700

    part 5

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
+	github.com/itsjamie/gin-cors v0.0.0-20160420130702-97b4a9da7933 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	gorm.io/driver/mysql v1.0.1
 	gorm.io/driver/postgres v1.0.1
--- server.go
+++ server.go
@@ -4,12 +4,29 @@ import (
 	"fmt"
 	"main/api"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	cors "github.com/itsjamie/gin-cors"
 )
 
 func main() {
 	router := gin.Default()
+	// Set up CORS middleware options
+	config := cors.Config{
+		Origins:        "*",
+		RequestHeaders: "Origin, Authorization, Content-Type",
+
+		Methods:         "GET, POST, PUT, DELETE",
+		Credentials:     true,
+		ValidateHeaders: false,
+		MaxAge:          1 * time.Minute,
+	}
+
+	// Apply the middleware to the router (works on groups too)
+	router.Use(cors.Middleware(config))
+
+	
 	router.Static("/images", "./uploaded/images")
 
 	api.Setup(router)
